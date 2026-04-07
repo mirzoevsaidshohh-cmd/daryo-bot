b943024838b8faed74d49f47f15bb5a5b6e3a2f5
mirzoevsaidshohh-cmd <mirzoevsaidshohh@gmail.com>
2026-04-07 08:42:17 +0500

    main.py

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ from threading import Thread
 
 # ---------- CONFIG ----------
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("8793536113:AAHwEQ-NNfyG9W4YbkrG6HdOpJFVXx_OeDY")
 if not TOKEN:
-    raise ValueError("TOKEN ёфт нашуд")
+    raise ValueError("8793536113:AAHwEQ-NNfyG9W4YbkrG6HdOpJFVXx_OeDY")
 
 BASE_URL = f"https://api.telegram.org/bot{TOKEN}/"
 DB_PATH = "daryo_telegram_bot.db"
